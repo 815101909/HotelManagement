ad0942a4d554a00599bfae9005c600d8906b6924
815101909 <815101909@qq.com>
2025-05-29 01:12:10 +0800

    进一步优化筛选问题（今日）

--- target/classes/static/admin/src/components/FinancialReports.vue
+++ target/classes/static/admin/src/components/FinancialReports.vue
@@ -467,7 +467,11 @@
   // 获取当天日期作为默认值
   const today = new Date()
   const formatDefaultDate = (date) => {
-    return date.toISOString().split('T')[0] // 格式: YYYY-MM-DD
+    // return date.toISOString().split('T')[0] // 格式: YYYY-MM-DD
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
   }
   
   // 财务数据
